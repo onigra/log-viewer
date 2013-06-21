c623af0b18a01ab9049d627e67733cf2292a6863
onigra <3280467rec@gmail.com>
2013-06-21 16:21:37 +0900

    サイドバーに現在登録されているサーバーの件数を標示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_tag_list
+
+  def set_tag_list
+    @tags = SlowLog.group("server").count
+  end
 end
