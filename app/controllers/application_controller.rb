df8a31703bc3f32bbbe3f183854821e3d12e1c57
onigra <3280467rec@gmail.com>
2013-07-02 18:39:57 +0900

    不要なので削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :set_tag_list, :request_from
+  before_filter :set_tag_list
 
   def set_tag_list
     @tags = SlowLog.group("server").count
-  end
-
-  def request_from
-    session[:return_to] ||= request.referer
   end
 end
